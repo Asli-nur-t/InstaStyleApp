f25002ef20056f3029f22fa8e45efe9c9128de2a
Aslınur Topcu <asl.nurtopcu13@gmail.com>
2023-07-27 15:12:59 +0300

    Initial Commit

---
+++ instaCloneProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaCloneProject
+//
+//  Created by Aslınur Topcu on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
